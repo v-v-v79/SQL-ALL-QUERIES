1d0d7ed796bb23c05eec84e59a3e17a773dfb8c9
mehmetCt <mehmet@cybertekschool.com>
2022-11-14 14:18:32 -0500

    Select is DONE

--- src/test/resources/Queries/day01/select.sql
+++ src/test/resources/Queries/day01/select.sql
@@ -34,4 +34,24 @@ select FIRST_NAME from employees;
 -- display firstname,lastname and salary from employees table
 select first_name,last_name,salary  from employees;
 
+-- display all information from locations table
+select * from locations;
+
+-- display country name and region id from countries table
+select country_name,region_id from countries;
+
+-- display job_id and job title from jobs table
+select job_id,job_title from jobs;
+
+
+
+
+
+
+
+
+
+
+
+
 
