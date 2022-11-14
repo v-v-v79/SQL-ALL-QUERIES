f89fe9b90444de792911849bc619a02809d2e3a3
mehmetCt <mehmet@cybertekschool.com>
2022-11-14 13:09:57 -0500

    Inital

---
+++ src/test/resources/Queries/day01/select.sql
@@ -0,0 +1,37 @@
+
+
+/*
+
+ This is multi line comments
+
+    MAC users
+    - Click over query
+    - CMD+ENTER
+
+    Win users
+    - Click over query
+    - CTRL + ENTER
+
+    Make sure you choosed line that you need to run
+
+ */
+-- DONT USE CLASS-CHAT TO SHARE SOLUTIONS
+
+select * from employees;
+-- Single line comments
+-- SQL language is NOT CASE SENSITIVE
+
+select * FROM eMpLoYeeS;
+
+-- display all information from departments table
+select * from departments;
+
+-- display firstname from employees table
+select first_name from employees;
+
+select FIRST_NAME from employees;
+
+-- display firstname,lastname and salary from employees table
+select first_name,last_name,salary  from employees;
+
+
