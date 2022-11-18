980172dea20a5c80464849c85a7580f81d3a6c6f
mehmetCt <mehmet@cybertekschool.com>
2022-11-18 11:15:14 -0500

    customer_address.sql

--- src/test/resources/Queries/day04/customer_address.sql
+++ src/test/resources/Queries/day04/customer_address.sql
@@ -12,6 +12,8 @@ INSERT INTO address (address_id, address, phone) VALUES (8,'1566 Inegl Manor',  
 INSERT INTO address (address_id, address, phone) VALUES (10,'1795 Santiago',  86045262);
 INSERT INTO address (address_id, address, phone) VALUES (11,'900 Santiago',  16571220);
 
+select * from address;
+
 CREATE TABLE customer(
                          customer_id Integer PRIMARY KEY,
                          first_name VARCHAR(50) NOT NULL,
@@ -24,5 +26,11 @@ INSERT INTO customer (customer_id, first_name, last_name,address_id) VALUES (2, 
 INSERT INTO customer (customer_id, first_name, last_name,address_id) VALUES (3,  'Linda' ,  'Williams' ,  7);
 INSERT INTO customer (customer_id, first_name, last_name,address_id) VALUES (4, 'Barbara' ,  'Jones' , 8);
 INSERT INTO customer (customer_id, first_name, last_name,address_id) VALUES (5,  'Elizabeth' ,  'Brown' ,  NULL);
+select * from customer;
+
+
 
 commit work;
+
+select * from EMPLOYEES;-- same --> commit
+select * from departments;
