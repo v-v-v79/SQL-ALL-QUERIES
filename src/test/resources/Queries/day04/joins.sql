3658708b9302723028b3c0669779e1823b7f0da1
mehmetCt <mehmet@cybertekschool.com>
2022-11-18 11:55:39 -0500

    left/right join

--- src/test/resources/Queries/day04/joins.sql
+++ src/test/resources/Queries/day04/joins.sql
@@ -33,5 +33,37 @@ select FIRST_NAME,LAST_NAME,C.ADDRESS_ID,A.ADDRESS_ID,ADDRESS,PHONE
 from ADDRESS A  inner join CUSTOMER C
                         on A.ADDRESS_ID = C.ADDRESS_ID;
 
+/*
+ LEFT OUTER JOIN / LEFT JOIN
+
+ -- it will give  matching part + unique for LEFT table
+ -- The order of is important
+
+ */
+select FIRST_NAME,LAST_NAME,ADDRESS,PHONE
+    from CUSTOMER C left outer join ADDRESS A
+        on C.ADDRESS_ID = A.ADDRESS_ID;
+
+select FIRST_NAME,LAST_NAME,ADDRESS,PHONE
+    from ADDRESS A left outer join CUSTOMER C
+    on A.ADDRESS_ID = C.ADDRESS_ID;
+
+/*
+ RIGHT OUTER JOIN / RIGHT JOIN
+
+ -- it will give  matching part + unique for RIGHT table
+ -- The order of is important
+
+ */
+select FIRST_NAME,LAST_NAME,ADDRESS,PHONE
+    from customer c right outer join address a
+    on c.ADDRESS_ID = a.ADDRESS_ID;
+
+select FIRST_NAME,LAST_NAME,ADDRESS,PHONE
+    from address a right join CUSTOMER c
+    on a.ADDRESS_ID = c.ADDRESS_ID;
+
+
+<
 
 
