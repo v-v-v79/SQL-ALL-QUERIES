2160b89de7e8b3f6e91626f8b338857743a0c032
mehmetCt <mehmet@cybertekschool.com>
2022-11-21 12:34:15 -0500

    JDBC Test Connection

--- src/test/java/com/cydeo/jdbctests/TestOracleConnection.java
+++ src/test/java/com/cydeo/jdbctests/TestOracleConnection.java
@@ -25,10 +25,47 @@ public class TestOracleConnection {
             Statement statement = conn.createStatement();
 
             // ResultSet will store data after execution.It stores only data (there is no table info )
-            ResultSet rs = statement.executeQuery("select * from employees");
+            ResultSet rs = statement.executeQuery("select * from regions");
+
+            /*
+            How to run query from Java file
+                - Click over Query (dont choose anywhere )
+                    - MAC --> CMD+ENTER
+                    - WIN --> CTRL+ENTER
+
+                - Choose session
+
+             */
 
 
             // GETTING DATA
+            // next() ---> Moves the cursor forward one row from its current position.
+
+            rs.next();
+
+            // pointer shows the first row
+
+            // how to get data
+
+                // rs.getInt(index)  --> it returns integer. Index start from 1 into SQL.It refers column position
+
+                    // it returns int
+                    System.out.println(rs.getInt(1));  // 1
+                    // it returns String
+                    System.out.println(rs.getString(1));  // 1
+
+                // rs.getInt(columLabel)
+                    System.out.println(rs.getInt("REGION_ID"));
+                    System.out.println(rs.getString("REGION_ID"));
+
+
+
+
+
+
+
+
+
 
 
 
