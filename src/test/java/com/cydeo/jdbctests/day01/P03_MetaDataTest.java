c3ac4bdb1c6b34124145f54372ebdfc2bd505122
mehmetCt <mehmet@cybertekschool.com>
2022-11-21 15:51:59 -0500

    MetaData

--- src/test/java/com/cydeo/jdbctests/day01/P03_MetaDataTest.java
+++ src/test/java/com/cydeo/jdbctests/day01/P03_MetaDataTest.java
@@ -20,16 +20,32 @@ public class P03_MetaDataTest {
         Statement statement = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
 
         // ResultSet will store data after execution.It stores only data (there is no table info )
-        ResultSet rs = statement.executeQuery("select * from REGIONS");
+        ResultSet rs = statement.executeQuery("select * from EMPLOYEES");
 
 
+        //  DatabaseMetaData  --> It is information about database itself
+        DatabaseMetaData dbMetaData = conn.getMetaData();
 
 
+        System.out.println(dbMetaData.getUserName());
+        System.out.println(dbMetaData.getDatabaseProductName());
+        System.out.println(dbMetaData.getDatabaseProductVersion());
+        System.out.println(dbMetaData.getDriverName());
+        System.out.println(dbMetaData.getDriverVersion());
+
+        
+        // ResultSetMetaData --> it provides information about table upper side (columnNames, columnCount)
+        ResultSetMetaData rsmd  = rs.getMetaData();
 
 
+        // How man column we have
+        int columnCount = rsmd.getColumnCount();
+        System.out.println(columnCount);
 
 
-
+        // get me column name from index 3
+        System.out.println(rsmd.getColumnName(3));
+        // it will return provided columnIndex name
 
 
         // close connections
