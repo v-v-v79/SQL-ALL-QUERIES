0b888e021a0600cf709de2b2949e48761808f574
mehmetCt <mehmet@cybertekschool.com>
2022-11-21 14:56:53 -0500

    Flexible Nav

--- src/test/java/com/cydeo/jdbctests/day01/P02_FlexibleNavigation.java
+++ src/test/java/com/cydeo/jdbctests/day01/P02_FlexibleNavigation.java
@@ -20,15 +20,30 @@ public class P02_FlexibleNavigation {
         Connection conn = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
 
         // It helps us to execute queries
-        Statement statement = conn.createStatement();
+        Statement statement = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
 
         // ResultSet will store data after execution.It stores only data (there is no table info )
         ResultSet rs = statement.executeQuery("select first_name,last_name from employees");
 
 
+        //first row
+        rs.next();
+        System.out.println(rs.getString(1)+" "+rs.getString(2));
 
 
+        //second row
+        rs.next();
+        System.out.println(rs.getString(1)+" "+rs.getString(2));
 
+        /*
+        ResultSet.TYPE_SCROLL_INSENSITIVE --> to do flexible navigation between rows
+        ResultSet.CONCUR_READ_ONLY        --> To dont update anything from database
+
+         */
+
+        // What if I want to get last data
+        rs.last();
+        System.out.println(rs.getString(1)+" "+rs.getString(2));
 
 
         // close connections
