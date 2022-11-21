1e3fa62fedb1b69915d81bdac27aa3a6327c6fbf
mehmetCt <mehmet@cybertekschool.com>
2022-11-21 10:51:59 -0500

    developers_testers.sql

--- src/test/resources/Queries/day05/developers_testers.sql
+++ src/test/resources/Queries/day05/developers_testers.sql
@@ -3,11 +3,15 @@ create table Developers(
    Names varchar(30),
    Salary Integer
 );
+
+
+
 create table Testers(
    Id_Number Integer primary key,
    Names varchar(30),
    Salary Integer
 );
+select * from Testers;
 
 insert into developers values (1, 'Mike', 155000);
 insert into developers values (2, 'John', 142000);
@@ -18,3 +22,89 @@ insert into testers values(2, 'Adam', 105000);
 insert into testers values (3, 'Lex', 100000);
 
 commit work;
+
+select * from testers;
+select * from developers;
+
+/*
+ UNION
+
+ - to merge more them one table vertically
+ - Remove DUPs
+ - Make an order as default asc
+
+
+ */
+
+select * from developers
+union
+select * from testers;
+
+
+select names from developers
+union
+select names from testers;
+
+
+select names from developers
+union
+select names from testers
+order by 1 desc ;
+
+
+/*
+
+ UNION ALL
+
+ - Dont Remove DUPS
+ - Dont sort the results
+ */
+
+select * from developers
+union all
+select * from testers;
+
+
+select names from developers
+union all
+select names from testers;
+
+
+-- if you wanna make an order use order by
+select names from developers
+union all
+select names from testers
+order by 1;
+
+/*
+MINUS
+ It removes same data from first table and gives result from first table
+
+ */
+
+
+select * from developers
+minus
+select * from testers;
+
+
+select names from developers
+minus
+select names from testers;
+
+select names from Testers
+minus
+select names from developers;
+
+/*
+ INTERSECT
+
+ */
+select * from developers
+intersect
+select * from testers;
+
+
+select names from developers
+intersect
+select names from testers;
