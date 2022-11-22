03644d32df5c754ed9c1f6ca5de117efc98dbc80
mehmetCt <mehmet@cybertekschool.com>
2022-11-22 12:37:45 -0500

    Library APP DB Test

---
+++ src/test/java/com/cydeo/jdbctests/day02/P02_Library_DBTest.java
@@ -0,0 +1,18 @@
+package com.cydeo.jdbctests.day02;
+
+import org.junit.jupiter.api.Test;
+
+public class P02_Library_DBTest {
+
+    // Create connection to MySQL Database
+    String url = "jdbc:mysql://34.230.35.214:3306/library2";
+    String username =  "library2_client";
+    String password =  "6s2LQQTjBcGFfDhY" ;
+    @Test
+    public void task1 () {
+
+
+
+
+    }
+}
---
+++ src/test/resources/Queries/jdbc/library.sql
@@ -0,0 +1,13 @@
+select * from books;
+
+
+-- if you cant connect
+-- delete existing one
+-- create from beginning
+-- maybe there is typo or extra spaces
+
+
+-- if you cant see tables
+-- REFRESH library2
+        -- right click over it
+        -- click REFRESH
